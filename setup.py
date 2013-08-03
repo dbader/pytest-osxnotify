7d840e1629034a1293eb5369e5e2461c7ac75b05
Daniel Bader <mail@dbader.org>
2013-08-03 13:58:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import setup
+from distutils.core import setup
+
+__VERSION__ = '0.1.0'
+__URL__ = 'https://github.com/dbader/pytest-osxnotify'
+__DOWNLOAD_URL__ = (__URL__ + '/tarball/' + __VERSION__)
+
 setup(
     author="Daniel Bader",
     author_email="mail@dbader.org",
-    version="0.1.0",
+    version=__VERSION__,
     description="OS X notifications for py.test results.",
+    url=__URL__,
+    download_url=__DOWNLOAD_URL__,
     name="pytest-osxnotify",
-    keywords="pytest, pytest-, osx, notifications, mountainlion, notificationcenter, py.test",
+    keywords=[
+        'pytest', 'pytest-', 'osx', 'notifications', 'mountainlion',
+        'notificationcenter', 'py.test'],
     packages=['pytest_osxnotify'],
-    entry_points={'pytest11': ['pyest_osxnotify = pytest_osxnotify', ]},)
+    entry_points={'pytest11': ['pyest_osxnotify = pytest_osxnotify', ]},
+    install_requires=[
+        "pyobjc-core",
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    ]
+)
