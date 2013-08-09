0405813ac9a4a9f554c20d4f0d90643431d712d6
Daniel Bader <mail@dbader.org>
2013-08-08 18:52:44 -0700

    Bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-__VERSION__ = '0.1.3'
+__VERSION__ = '0.1.4'
 __URL__ = 'https://github.com/dbader/pytest-osxnotify'
 __DOWNLOAD_URL__ = (__URL__ + '/tarball/' + __VERSION__)
 
